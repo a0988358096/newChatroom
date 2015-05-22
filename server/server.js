64e32607e7d05ab84eefc705b0dba2d1b6119d53
shimao <a0988358096@gmail.com>
2015-05-22 17:10:04 +0800

    try test go

--- server/server.js
+++ server/server.js
@@ -1,49 +1,8 @@
-var express = require('express'),
-	app = express(),
-	server = require('http').createServer(app),
-	io = require('socket.io').listen(server),
-	nicknames = [];
-
-server.listen(3000);
-
-app.get('/', function(req, res){
-	res.sendfile(__dirname + '/index.html');
-});
-app.use('/public', express.static(__dirname + '/public'));
-
-io.sockets.on('connection', function(socket) {
-	socket.on('new user', function(data){
-		console.log(data);
-		if (nicknames.indexOf(data) != -1) {
-
-		} else {
-			socket.emit('chat', 'SERVER', 'Welcome ' + data);
-
-			socket.nickname = data;
-			nicknames.push(socket.nickname);
-			io.sockets.emit('usernames', nicknames);
-			updateNicknames();
-		}
-	});
-
-	function updateNicknames(){
-		io.sockets.emit('usernames', nicknames);
-	}
-
-	//
-	socket.on('send message', function(data){
-		io.sockets.emit('new message', { msg: data, nick: socket.nickname });
-	});
-
-	socket.on('disconnect', function(data){
-		if (!socket.nickname) return;
-		io.sockets.emit('chat', 'SERVER', socket.nickname + ' has leaved the chatroom!');
-		nicknames.splice(nicknames.indexOf(socket.nickname), 1);
-		updateNicknames();
-	});
-});
-
 (function () {
+    var connectedClients = {}, //used to keep a working list of the connections
+        io = require('socket.io').listen(4000);
+
+        
 
     io.sockets.on('connection', function (socket) {
 
